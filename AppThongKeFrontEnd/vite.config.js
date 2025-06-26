9382c49da76ff2e4d1463509b7e484fcc9c96a94
khannps39199 <khannps39199@gmail.com>
2025-06-26 14:03:33 +0700

    Update vite.config.js

--- AppThongKeFrontEnd/vite.config.js
+++ AppThongKeFrontEnd/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/lllloc123h.github.io/',
+  base: '/',
   server: { // Thêm phần server này vào
     proxy: {
       '/api-provinces': {
