249d0045a04581e58c3620611f78a4336beb27dd
khannps39199 <khannps39199@gmail.com>
2025-06-26 14:22:35 +0700

    update

--- AppThongKeFrontEnd/src/App.vue
+++ AppThongKeFrontEnd/src/App.vue
@@ -3,15 +3,13 @@ import Login from './components/Login.vue';
 import loaiVanban from './components/DanhMuc/loaivanBan.vue';
 import SidebarMenu from './components/SidebarMenu.vue';
 import Header from './components/header.vue';
-import { RouterView } from 'vue-router';
+// import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <Header></Header>
   <div>
-    <RouterView></RouterView>
-
+    <router-view></router-view>
   </div>
-
 </template>
 <script setup></script>
